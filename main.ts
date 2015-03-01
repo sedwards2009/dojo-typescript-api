f43d5e5a1fa0a25d1869c969f5a82d16aa9a0fd8
Simon Edwards <simon@simonzone.com>
2015-03-01 20:11:06 +0100

    Process the complete API file, not a small version of it.

--- main.ts
+++ main.ts
@@ -5,8 +5,7 @@ import fs = require('fs');
 import generator = require('./generator');
 import DojoDetailsInterface = require('./DojoDetailsInterface');
 
-/*const DETAILS_FILENAME = 'data/details-1.10.json';*/
-const DETAILS_FILENAME = 'data/small-details-1.10.json';
+const DETAILS_FILENAME = 'data/details-1.10.json';
 
 function main() {
   console.log("Reading ", DETAILS_FILENAME);
