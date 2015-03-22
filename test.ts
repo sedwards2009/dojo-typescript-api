1968581ef8e453f5d019ebbb1b83584249453857
Simon Edwards <simon@simonzone.com>
2015-03-22 20:55:42 +0100

    Removed some old partial unit tests. Added some compile tests which are actually unit tests from the Dojo source itself. Not all of the test pass at the moment.

--- test.ts
+++ test.ts
@@ -4,22 +4,20 @@
 // Unittests for use with nodeunit https://github.com/caolan/nodeunit
 
 import nodeunit = require('nodeunit');
-import fs = require('fs');
-import path = require('path');
 import child_process = require('child_process');
 import shelljs = require('shelljs');
 
 import generate_all = require('./generate_all');
 import generator = require('./generator');
-import DojoDetailsInterface = require('./DojoDetailsInterface');
 
 // Utility functions.
 
-const DETAILS_FILENAME = 'data/details-1.10.json';
-const details = readDetails(DETAILS_FILENAME);
+// const DETAILS_FILENAME = 'data/details-1.10.json';
 const TMP_DIR = "target/tmp";
-const HEADER_FILENAME = "header.d.ts";
-const CODE_FILENAME = "code.ts";
+// const HEADER_FILENAME = "header.d.ts";
+// const CODE_FILENAME = "code.ts";
+
+generate_all.main();
 
 // Clean tmp before each test.
 export function setUp(callback: Function): void {
@@ -27,22 +25,14 @@ export function setUp(callback: Function): void {
     shelljs.rm("-rf", TMP_DIR);
   }
   shelljs.mkdir(TMP_DIR);
+  
 
   callback();
 }
 
-function readDetails(filename: string): DojoDetailsInterface.DojoDetailsInterface {
-  const details: DojoDetailsInterface.DojoDetailsInterface = <DojoDetailsInterface.DojoDetailsInterface> JSON.parse(fs.readFileSync(filename, 'utf8'));
-  return details;
-}
-
-function tsCompile(headerText: string, codeText: string): boolean {
-  fs.writeFileSync(path.join(TMP_DIR, HEADER_FILENAME), headerText, { encoding: "utf8"});
-
-  const completeCode = `/// <reference path="./${HEADER_FILENAME}" />\n${codeText}`;
-  fs.writeFileSync(path.join(TMP_DIR, CODE_FILENAME), completeCode, { encoding: "utf8"});
+function testCompile(filename: string): boolean {
   try {
-    child_process.execSync(`tsc --target es6 --module commonjs --outDir ${TMP_DIR} ${path.join(TMP_DIR, CODE_FILENAME)}`);
+    child_process.execSync(`tsc --target es6 --module commonjs --outDir ${TMP_DIR} ${filename}`);
   } catch(e) {
     console.log(formatChildProcessError(e));
     return false;
@@ -54,45 +44,12 @@ function formatChildProcessError(err: {stdout: Buffer[]}): string {
   return "Error:\n" + err.stdout.toString();
 }
 
-function compileTest(test: nodeunit.Test, moduleNames: string[]): void {
-  var textDetails: DojoDetailsInterface.DojoDetailsInterface = {};
-
-  moduleNames.forEach( (name) => {
-    textDetails[name] = details[name];
-  } );
-
-  const seenModules = new Set<string>();
-  
-  const importText = moduleNames.map( (name) => {
-    let normalizedName = name.replace(/[/.-]/g,"_");
-    if (seenModules.has(normalizedName)) {
-      normalizedName += "_";
-    }
-    seenModules.add(normalizedName);
-    return "import " + normalizedName + " = require('" + name + "');\n"
-  } ).join("");
-  const headerText = generator.formatAPI(textDetails);
-
-  test.ok(tsCompile(headerText, importText));
-  test.done();
-}
-
 //-------------------------------------------------------------------------
 // Tests begin here.
-
-// A simple compile test of a tiny part of the Dojo API.
-export function testDojoString(test: nodeunit.Test): void {
-  compileTest(test, ["dojo/string"]);
-}
 
 export function testReturnTypePromise(test: nodeunit.Test): void {
   test.equal(generator.formatType("dojo/promise/Promise"), "dojo.promise.Promise");
   test.done();
-}
-
-export function testDojoRequest(test: nodeunit.Test): void {
-  compileTest(test, ["dojo/request", "dojo/request.__BaseOptions", "dojo/request.__MethodOptions",
-    "dojo/request.__Options", "dojo/request.__Promise", "dojo/promise/Promise"]);
 }
 
 export function testNormalizeDash(test: nodeunit.Test): void {
@@ -100,9 +57,22 @@ export function testNormalizeDash(test: nodeunit.Test): void {
   test.done();
 }
 
-export function testDojoModule(test: nodeunit.Test): void {
-  generate_all.main();
-  const headerText = fs.readFileSync("output/dojo.d.ts", "utf8");
-  tsCompile(headerText, "");
+export function testCompileDateStamp(test: nodeunit.Test): void {
+  test.ok( testCompile("test_files/date_stamp.ts") );
+  test.done();
+}
+
+export function testCompileDojoDateLocale(test: nodeunit.Test): void {
+  test.ok( testCompile("test_files/date_locale.ts") );
+  test.done();
+}
+
+export function testCompileDojoStoreDatastore(test: nodeunit.Test): void {
+  test.ok( testCompile("test_files/dojo_store_datastore.ts") );
+  test.done();
+}
+
+export function testCompileDojo_BaseLang(test: nodeunit.Test): void {
+  test.ok( testCompile("test_files/dojo__base_lang.ts") );
   test.done();
 }
---
+++ test_files/test_harness.d.ts
@@ -0,0 +1,19 @@
+declare var assert: {
+  equal(a:any, b:any): any;
+  instanceOf(a:any, b:any): any;
+  isNull(a:any): any;
+  ok(a: boolean): any;
+  isTrue(a: boolean): any;
+  isFalse(a: boolean): any;
+};
+
+declare function registerSuite(x: any): void;
+
+
+
+declare var doh: {
+  register(...a: any[]): any;
+  Deferred: {
+    new(): any;
+  };
+};
